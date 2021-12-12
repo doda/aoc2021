8addd721baaf98deaf94c664d75fb70890a9c172
Dominik Dabrowski <dominik+github@doda.co>
2021-12-12 12:45:20 -0800

    Refactor day 1

--- src/commands/solutions/day01.ts
+++ src/commands/solutions/day01.ts
@@ -2,23 +2,6 @@ import { GluegunCommand } from 'gluegun'
 
 const DAY = '01'
 
-const rolling = (array: Array<any>, window: Number) => {
-  let result = []
-  let curWindow = []
-
-  for (let value of array) {
-    if (curWindow.length < window) {
-      curWindow.push(value)
-      continue
-    }
-    result.push([...curWindow])
-    curWindow = curWindow.slice(1)
-    curWindow.push(value)
-  }
-  result.push([...curWindow])
-  return result
-}
-
 const part1 = (input: Array<number>) => {
   let result = 0
   let prevNum = Number.MAX_SAFE_INTEGER
@@ -31,15 +14,8 @@ const part1 = (input: Array<number>) => {
 }
 
 const part2 = (input: Array<number>) => {
-  let result = 0
-  let lastSum = Number.MAX_SAFE_INTEGER
-  rolling(input, 3).map(triple => {
-    let [a, b, c] = triple
-    let newSum = a + b + c
-    if (newSum > lastSum) result += 1
-    lastSum = newSum
-  })
-  return result
+  let triples = input.slice(2).map((x, i) => x + input[i] + input[i + 1])
+  return triples.slice(1).reduce((a, x, i) => a + (triples[i] < x ? 1 : 0), 0)
 }
 
 const command: GluegunCommand = {
@@ -47,7 +23,7 @@ const command: GluegunCommand = {
   run: async toolbox => {
     const { print, readInput } = toolbox
     let input = readInput(DAY)
-    let numInput = input.map(line => Number(line))
+    let numInput = input.map(line => parseInt(line))
 
     print.info(`Part 1: ${part1(numInput)}`)
     print.info(`Part 2: ${part2(numInput)}`)
